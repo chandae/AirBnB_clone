60b502185e0f54c63d56523db409a0fae923cabd
chandae <emmanuelvchanda1@gmail.com>
2022-08-06 20:57:40 +0200

    Updating console

--- console.py
+++ console.py
@@ -7,8 +7,15 @@
 
 import cmd
 import models
-from models import storage
+import re
+from models.amenity import Amenity
 from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -229,10 +236,76 @@ class HBNBCommand(cmd.Cmd):
         """
         return True
 
-    def default(self, line: str) -> None:
+    # ----- extended console commands -----
+    def default(self, line: str):
         """
             Invoked when command is not recognised
         """
+
+        # Catch any invalid command and input
+        try:
+            cmds = line.split(".")
+            eval(cmds[0])
+        except Exception as e:
+            return
+
+        # Retrieve object by class name and id <class_name>.show(<id>)
+        if 'show' in cmds[1]:
+            # Retrieve object by id
+            id = cmds[1].lstrip('show("').rstrip('")')
+            self.do_show(f"{cmds[0]} {id}")
+            return
+        elif 'destroy' in cmds[1]:
+            # Delete object by id
+            id = cmds[1].lstrip('destory("').rstrip('")')
+            self.do_destroy(f"{cmds[0]} {id}")
+            return
+
+        # Return all objects belonging to the given class
+        cmd_ptn = re.compile(r"\w.all")
+        result = cmd_ptn.search(line)
+        if result:
+            self.do_all(cmds[0])
+            return
+
+        # Count the number of objects of the given class
+        cmd_ptn = re.compile(r"\w.count()")
+        if cmd_ptn.search(line):
+            models.storage.reload()
+            objs = storage.all()
+            output = [f"{str(objs[key])}" for key in objs if cmds[0] in key]
+            print(len(output))
+            return
+
+    def help_create(self):
+        msg = """
+        Creates a new instance of BaseModel and saves it.
+        Usage: create <ClassName>
+        """
+        print(msg)
+
+    def help_show(self):
+        print("Prints the string representation of an instance\
+based on the class name and id.\nUsage: show <ClassName> <id>.")
+
+    def help_all(self):
+        print("Prints all string representation of all instances.\
+\nUsage: all or all <ClassName>")
+
+    def help_update(self):
+        print("Updates an instance based on the class name and id\
+by adding or changing attribute values.\
+\nUsage: update <class name> <id> <attribute name> \"<attribute value>\"")
+
+    def help_count(self):
+        print("Prints the number of instances of a class.\
+\nUsage: <class_name>.count() ")
+
+    def help_quit(self):
+        print("Exit the command line.\nUsage: quit")
+
+    def help_EOF(self):
+        print("Exit the command line")
 
 
 if __name__ == '__main__':
